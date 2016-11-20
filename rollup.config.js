c46905c126fa83a18e777434c3d7a661c7d89568
Rich-Harris <richard.a.harris@gmail.com>
2016-11-20 13:36:07 -0500

    fix deploy process

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,14 @@
 import svelte from 'rollup-plugin-svelte';
+import buble from 'rollup-plugin-buble';
+import uglify from 'rollup-plugin-uglify';
+
+const plugins = [ svelte() ];
+if ( process.env.production ) plugins.push( buble(), uglify() );
 
 export default {
 	entry: 'src/app.js',
 	dest: 'dist/bundle.js',
 	format: 'iife',
-	plugins: [
-		svelte()
-	],
+	plugins,
 	sourceMap: true
 };
