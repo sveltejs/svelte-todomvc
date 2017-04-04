767b83c909bb4a3a3d79207fe59f051ee9253e88
Rich Harris <richard.a.harris@gmail.com>
2017-04-04 11:19:17 -0400

    lock CSS to working version, update README, other chores

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,19 @@ import buble from 'rollup-plugin-buble';
 import closure from 'rollup-plugin-closure-compiler-js';
 import filesize from 'rollup-plugin-filesize';
 
+// you can use UglifyJS instead of Closure — the result will be ~3.3kb
+// import uglify from 'rollup-plugin-uglify';
+
 const plugins = [ svelte() ];
-if ( process.env.production ) plugins.push( buble(), closure(), filesize() );
+if ( process.env.production ) {
+	plugins.push(
+		buble(),
+		closure({
+			compilationLevel: 'ADVANCED'
+		}),
+		filesize()
+	);
+}
 
 export default {
 	entry: 'src/main.js',
